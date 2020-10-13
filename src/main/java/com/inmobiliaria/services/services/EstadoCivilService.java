f10d7d9313fd0896b160672f6edcafa0b131548e
Walter Raul Canchan Hermoza <wcanchan@gmail.com>
2020-10-12 22:16:57 -0500

    add

--- src/main/java/com/inmobiliaria/services/services/EstadoCivilService.java
+++ src/main/java/com/inmobiliaria/services/services/EstadoCivilService.java
@@ -12,7 +12,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.inmobiliaria.services.modelv1.EstadoCivil;
+import com.inmobiliaria.services.model.EstadoCivil;
 import com.inmobiliaria.services.repository.EstadoCivilRepository;
 
 @Service
