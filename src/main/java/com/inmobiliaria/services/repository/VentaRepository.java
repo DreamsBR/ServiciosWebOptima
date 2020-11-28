60d1de08cfa8e9f2c924e6fb76ad8390e16868ae
Walter Raul Canchan Hermoza <wcanchan@gmail.com>
2020-11-28 03:35:55 -0500

    ci

--- src/main/java/com/inmobiliaria/services/repository/VentaRepository.java
+++ src/main/java/com/inmobiliaria/services/repository/VentaRepository.java
@@ -13,7 +13,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import com.inmobiliaria.services.model.Cliente;
 import com.inmobiliaria.services.model.Venta;
 @Repository
 public interface VentaRepository extends JpaRepository<Venta, Integer> {
